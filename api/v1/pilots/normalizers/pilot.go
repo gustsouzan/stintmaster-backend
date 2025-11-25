cd0e20afb039ea9ce68c9c139bb5698c10f8498c
gustsouzan <gustsouzan@gmail.com>
2025-11-25 09:31:12 -0300

    Refactor event and pilot handlers to return detailed error messages; update data structures for pilots and events to include new fields and improve filtering logic.

--- api/v1/pilots/normalizers/pilot.go
+++ api/v1/pilots/normalizers/pilot.go
@@ -2,9 +2,11 @@ package normalizers
 
 type PostPilot struct {
 	Name       string `json:"name" validate:"required"`
-	Age        int    `json:"age" validate:"required,gte=0"`
-	Experience int    `json:"experience" validate:"required"`
+	Email      string `json:"email"`
+	Age        string `json:"age" validate:"required,gte=0"`
+	Experience string `json:"experience" validate:"required"`
 	Team       string `json:"team" validate:"required"`
 	IracingID  string `json:"iracing_id" validate:"required"`
+	ImageURL   string `json:"image_url"`
 	CreatedBy  string `json:"created_by" validate:"required"`
 }
--- integrations/postgres/events.go
+++ integrations/postgres/events.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	"database/sql"
 	"log"
+	"strconv"
 
 	"stintmaster/api/domains/events/normalizers"
 )
@@ -25,6 +26,63 @@ func GetEvents(conn *sql.DB) (events []normalizers.Event, err error) {
 	query := `SELECT id, name, platform, date, duration, image_url, created_by, created_at FROM event`
 
 	rows, err := conn.Query(query)
+	if err != nil {
+		log.Println("Error executing query:", err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var event normalizers.Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Platform, &event.Date, &event.Duration, &event.ImageURL, &event.CreatedBy, &event.CreatedAt)
+		if err != nil {
+			log.Println("Error scanning row:", err)
+			return nil, err
+		}
+		events = append(events, event)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Println("Row iteration error:", err)
+		return nil, err
+	}
+
+	return events, nil
+}
+
+func GetEventsByFilter(conn *sql.DB, filter normalizers.Event) (events []normalizers.Event, err error) {
+
+	query := `SELECT id, name, platform, date, duration, image_url, created_by, created_at FROM event WHERE 1=1`
+	var args []interface{}
+	argID := 1
+
+	if filter.Name != "" {
+		query += ` And (name ILIKE $` + strconv.Itoa(argID)
+		args = append(args, "%"+filter.Name+"%")
+		argID++
+	}
+	if filter.Platform != "" {
+		query += ` And platform ILIKE $` + strconv.Itoa(argID)
+		args = append(args, "%"+filter.Platform+"%")
+		argID++
+	}
+	if !filter.Date.IsZero() {
+		query += ` And date = $` + strconv.Itoa(argID)
+		args = append(args, filter.Date)
+		argID++
+	}
+	if filter.Duration != 0 {
+		query += ` And duration = $` + strconv.Itoa(argID) + `)`
+		args = append(args, filter.Duration)
+		argID++
+	}
+	if filter.CreatedBy != "" {
+		query += ` or created_by ILIKE $` + strconv.Itoa(argID)
+		args = append(args, "%"+filter.CreatedBy+"%")
+		argID++
+	}
+
+	rows, err := conn.Query(query, args...)
 	if err != nil {
 		log.Println("Error executing query:", err)
 		return nil, err
